8bea2fbc6128f4f377033c22cbfa13c68f6d06f2
Ville Peurala <ville.peurala@gmail.com>
2017-11-19 16:55:10 +0200

    Does not crash.

--- lib/dockerfolder.rb
+++ lib/dockerfolder.rb
@@ -1,9 +1,14 @@
 require "dockerfolder/version"
+require "optparse"
 
 module Dockerfolder
   class Dockerfolder
     def initialize(argv)
       puts "Dockerfolder created!"
+      validate(argv)
+    end
+
+    def validate(argv)
     end
 
     def run
