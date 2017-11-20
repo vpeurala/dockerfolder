e7f129a19a902446eaf9943e0f4eb7bf8e536cde
Ville Peurala <ville.peurala@gmail.com>
2017-11-20 17:13:32 +0200

    Write descriptions for the options.

--- lib/dockerfolder/command_line_args.rb
+++ lib/dockerfolder/command_line_args.rb
@@ -7,17 +7,23 @@ class CommandLineArgs
   def parse_command_line(argv)
     options = {}
     opt_parser = OptionParser.new do |opts|
-      opts.banner = "Usage: #{$0} [options]"
+      opts.banner = "Usage: #{$PROGRAM_NAME} [options]"
 
-      opts.on("-c", "--container-name CONTAINER_NAME") do |container_name|
+      opts.on("-b", "--base-image IMAGE_NAME", "The Docker base image on which your Dockerfile is built. For example: --base-image='postgres:10-alpine'. This will be value of the FROM instruction in the generated Dockerfile if you don't supply an existing Dockerfile to #{$PROGRAM_NAME}.") do |image_name|
+        options[:image_name] = image_name
+      end
+
+      opts.on("-c", "--container-name CONTAINER_NAME", "The name of the Docker container which will be built by the generated scripts. For example: --container-name='amqp-example'.") do |container_name|
         options[:container_name] = container_name
       end
 
-      opts.on("-d", "--directory directory_name") do |directory_name|
-        options[:directory_name] = directory_name
+      opts.on("-f", "--docker-file DOCKER_FILE", "The path to an already existing Dockerfile which you want to use. The file will be copied to the resulting folder of scripts. For example: --docker-file='/Users/vpeurala/templates/postgresql/Dockerfile'.")
+
+      opts.on("-d", "--directory directory_path", "The path to the directory which will be created. For example: --directory='scripts/dockerfolder' The default is ''./docker'.") do |directory_path|
+        options[:directory_path] = directory_path
       end
 
-      opts.on("-i", "--image-name IMAGE_NAME") do |image_name|
+      opts.on("-i", "--image-name IMAGE_NAME", "The name of the Docker image which will be built by the generated scripts from the Dockerfile. For example: --image-name='vpeurala/amqp-example:latest'.") do |image_name|
         options[:image_name] = image_name
       end
     end
